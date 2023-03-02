ad652d271df59ec1b592ca27724638c808b82692
Yahya Essam <yahyaessam2009@gmail.com>
2023-03-02 20:34:49 +0200

    Create BinarySearchTree.js

---
+++ BinarySearchTree.js
@@ -0,0 +1,64 @@
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+
+    }
+
+    insert(value) {
+        const newNode = new TreeNode(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this
+        } else {
+
+            let currentNode = this.root
+            while (true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                }
+
+            }
+        }
+    }
+    lookUp(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else if (value === currentNode.value) {
+                return currentNode
+            }
+        }
+        return false;
+    }
+
+}
+
+const myTree = new BinarySearchTree();
+
